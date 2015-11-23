cd90376f75ab1415190e199b80a340a29bec9f99
rvh25 <rohainehsu@dhcp-hol-129.eduroam.cornell.edu>
2015-11-23 14:19:18 -0500

    view controller update

--- Airdroptest/ViewController.swift
+++ Airdroptest/ViewController.swift
@@ -12,12 +12,67 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func filecontent(filepath: String) {
+        let documentDirectoryURL = try! NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true)
+        
+        let fileDestinationUrl = documentDirectoryURL.URLByAppendingPathComponent("\(filepath)")
+        do {
+            let contentsOfFile = try NSString(contentsOfFile: fileDestinationUrl.path!, encoding: NSUTF8StringEncoding)
+            print("Content of file = \(contentsOfFile)")
+        } catch let error as NSError {
+            print(error)
+            print("No file found")
+        }
+    }
+    
+    func files() {
+        let documentsURL = try! NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true)
+        
+        do {
+            let directoryContents = try NSFileManager.defaultManager().contentsOfDirectoryAtURL(documentsURL.absoluteURL, includingPropertiesForKeys: nil, options: NSDirectoryEnumerationOptions())
+            print(directoryContents)
+            
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    @IBAction func Checkfiledirectory(sender: AnyObject) {
+        files()
+    }
+
+    
+    @IBAction func shareFile(sender: AnyObject) {
+        
+        /*let documentDirectoryURL = try! NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true)
+        let text = documentDirectoryURL.URLByAppendingPathComponent("Testfile.txt")*/
+
+        
+        let path = NSBundle.mainBundle().pathForResource("Testfile", ofType: "txt")
+        let text = try? NSString(contentsOfFile: path!, encoding: NSUTF8StringEncoding)
+        
+        let controller = UIActivityViewController(activityItems: [text!], applicationActivities: nil)
+
+        controller.excludedActivityTypes = [UIActivityTypePostToFacebook, UIActivityTypePostToTwitter, UIActivityTypePostToWeibo, UIActivityTypePrint, UIActivityTypeCopyToPasteboard, UIActivityTypeAssignToContact, UIActivityTypeSaveToCameraRoll, UIActivityTypePostToFlickr, UIActivityTypePostToTencentWeibo, UIActivityTypeMail]
+        
+        self.presentViewController(controller, animated: true, completion: nil)
+        
+        
+        if let pop = controller.popoverPresentationController {
+            let v = sender as! UIView
+            pop.sourceView = v
+            pop.sourceRect = v.bounds
+        }
     }
 
 
